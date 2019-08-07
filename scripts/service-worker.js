896a1462ed1b4fc22a962571a707812d5f13e4d7
Oleg Volkov <olegv@parallels.com>
2019-08-07 12:30:00 +0200

    debug

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -24,7 +24,7 @@ self.addEventListener('install', (evt) => {
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       console.log('[ServiceWorker] Pre-caching offline page');
-      return cache.addAll(FILES_TO_CACHE);
+      //return cache.addAll(FILES_TO_CACHE);
     })
   );
   self.skipWaiting();
